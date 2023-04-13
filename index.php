fe6e7499ae5dff7bf42c92f66ee7a253709579ee
aryanugraha45 <myjavalasca@gmail.com>
2023-04-13 10:10:13 +0700

    menambahka list padaindex.php

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi suscipit dolorem harum beatae velit quaerat aliquid obcaecati distinctio, facilis reprehenderit natus quos impedit et quo pariatur eligendi consectetur fugiat possimus.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
